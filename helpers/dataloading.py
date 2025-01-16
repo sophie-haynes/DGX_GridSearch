c934171c683590d5941d86b1b0ddc86380f77f9e
Sophie Haynes <s.c.haynes@rgu.ac.uk>
2025-01-16 15:17:57 +0000

    Patch: Remove Broken Function in dataloading Helper

--- helpers/dataloading.py
+++ helpers/dataloading.py
@@ -10,8 +10,3 @@ class ImageFolderWithPaths(ImageFolder):
         path = self.imgs[index][0]
 
         return (img, label, path)
-
-def remap_labels(label):
-    """Helper function to map class labels ['1_nodule', '0_normal'] to indices. """
-    mapping_dict = {trainset.class_to_idx['1_nodule']: 1, trainset.class_to_idx['0_normal']: 0}
-    return mapping_dict[label]

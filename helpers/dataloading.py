0119e130e93e7714d1950b1ecaa7a617f20cfb25
Sophie Haynes <s.c.haynes@rgu.ac.uk>
2025-01-16 15:59:33 +0000

    Add single_channel support to load_dataset..() to Dataloading helpers

--- helpers/dataloading.py
+++ helpers/dataloading.py
@@ -1,6 +1,6 @@
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-from helpers.cxr import get_cxr_dataset_normalisation
+from helpers.cxr import get_cxr_dataset_normalisation, get_cxr_eval_transforms, get_cxr_single_eval_transforms 
 
 class ImageFolderWithPaths(ImageFolder):
     """Modifies torchviison ImageFolder to return (img, label, img_path)"""
@@ -14,11 +14,18 @@ class ImageFolderWithPaths(ImageFolder):
         return (img, label, path)
 
 def load_dataset_with_paths(dataset_path, dataset_name, process="arch", 
-    crop_size=512, batch_size=4, shuffle=True):
+    crop_size=512, batch_size=4, shuffle=True, single_channel=False):
     """Wrapper helper to load a dataset with img paths."""
     dataset = ImageFolderWithPaths(
         root = dataset_path,
         transform = get_cxr_eval_transforms(
+            crop_size = crop_size,
+            normalise = get_cxr_dataset_normalisation(
+                dataset = dataset_name, 
+                process = process
+                )
+            ) if not single_channel else
+        get_cxr_single_eval_transforms(
             crop_size = crop_size,
             normalise = get_cxr_dataset_normalisation(
                 dataset = dataset_name, 
@@ -29,11 +36,18 @@ def load_dataset_with_paths(dataset_path, dataset_name, process="arch",
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
 def load_dataset(dataset_path, dataset_name, process="arch", 
-    crop_size=512, batch_size=4, shuffle=True):
+    crop_size=512, batch_size=4, shuffle=True, single_channel=False):
     """Wrapper helper to load a dataset."""
     dataset = ImageFolder(
         root = dataset_path,
         transform = get_cxr_eval_transforms(
+            crop_size = crop_size,
+            normalise = get_cxr_dataset_normalisation(
+                dataset = dataset_name, 
+                process = process
+                )
+            ) if not single_channel else
+        get_cxr_single_eval_transforms(
             crop_size = crop_size,
             normalise = get_cxr_dataset_normalisation(
                 dataset = dataset_name, 

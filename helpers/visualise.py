abf60996999ce96da2eb3f56aa6eace985ea947a
sophie-haynes <s.c.haynes@rgu.ac.uk>
2025-03-05 18:11:07 +0000

    Add Tensor Normalisation Method to helper.visualise

--- helpers/visualise.py
+++ helpers/visualise.py
@@ -8,3 +8,11 @@ def norm_and_transpose_input_tensor(curr_img_tensor):
                    (curr_img_tensor.cpu().numpy().max() -
                     curr_img_tensor.cpu().cpu().numpy()
                     .min())).transpose(1, 2, 0)
+
+def norm_input_tensor(curr_img_tensor):
+    """Normalises input image tensor for viz"""
+    return asarray((curr_img_tensor.cpu().numpy() -
+                    curr_img_tensor.cpu().numpy().min()) /
+                   (curr_img_tensor.cpu().numpy().max() -
+                    curr_img_tensor.cpu().cpu().numpy()
+                    .min()))
